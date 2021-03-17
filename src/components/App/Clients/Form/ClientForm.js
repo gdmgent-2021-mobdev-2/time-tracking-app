0793d768a61808744b79627110ca15e8e857905b
Michael Vanderpoorten <michael@yungpanda.com>
2021-03-17 17:16:14 +0100

    ♻️ Form hook

--- src/components/App/Clients/Form/ClientForm.js
+++ src/components/App/Clients/Form/ClientForm.js
@@ -1,8 +1,7 @@
-import { useState, useCallback, useEffect } from 'react';
 import Input from '../../../Design/Input';
 import Button from '../../../Design/Button';
 import * as yup from 'yup';
-import { getValidationErrors } from '../../../../core/utils/validation';
+import useForm from '../../../../core/hooks/useForm';
 
 const schema = yup.object().shape({
     email: yup.string().email().required(),
@@ -19,67 +18,43 @@ const defaultData = {
 }
 
 const ClientForm = ({ onSubmit, initialData = {}, disabled }) => {
-    const [isTouched, setIsTouched] = useState(false);
-    const [data, setData] = useState({
+    const initial = {
         ...defaultData,
         ...initialData,
-    });
-    const [errors, setErrors] = useState({});
-
-    const handleChange = (e) => {
-        setData({
-            ...data,
-            [e.target.name]: e.target.value,
-        });
     };
+    const {
+        values,
+        errors,
+        handleChange,
+        handleSubmit,
+    } = useForm(schema, initial);
 
-    const validate = useCallback((data, onSuccess) => {
-        schema.validate(data, { abortEarly: false })
-            .then(() => {
-                if (onSuccess) {
-                    onSuccess();
-                }
-            }).catch((err) => {
-            setErrors(getValidationErrors(err));
-        });
-    }, []);
-
-    useEffect(() => {
-        if (isTouched) {
-            validate(data);
-        }
-    }, [validate, isTouched, data]);
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        setIsTouched(true);
-        validate(data, () => {
-            onSubmit(data);
-        })
+    const handleData = (values) => {
+        onSubmit(values);
     };
 
     return (
-        <form onSubmit={handleSubmit} noValidate={true}>
+        <form onSubmit={handleSubmit(handleData)} noValidate={true}>
             <label htmlFor="company">Company</label>
             <Input type="text" name="company"
-                   value={data.company}
+                   value={values.company}
                    disabled={disabled}
                    onChange={handleChange}
                    error={errors.company}/>
 
             <label htmlFor="email">Email address</label>
-            <Input type="email" name="email" value={data.email} disabled={disabled} onChange={handleChange}
+            <Input type="email" name="email" value={values.email} disabled={disabled} onChange={handleChange}
                    error={errors.email}/>
 
             <label htmlFor="firstName">First Name</label>
-            <Input type="text" name="firstName" value={data.firstName} disabled={disabled} onChange={handleChange}
+            <Input type="text" name="firstName" value={values.firstName} disabled={disabled} onChange={handleChange}
                    error={errors.firstName}/>
 
             <label htmlFor="lastName">Last Name</label>
-            <Input type="text" name="lastName" value={data.lastName} disabled={disabled} onChange={handleChange}
+            <Input type="text" name="lastName" value={values.lastName} disabled={disabled} onChange={handleChange}
                    error={errors.lastName}/>
 
-            <Button type="submit" disabled={disabled}>{data._id ? 'Update' : 'Create'}</Button>
+            <Button type="submit" disabled={disabled}>{values._id ? 'Update' : 'Create'}</Button>
 
         </form>
     )
---
+++ src/core/hooks/useForm.js
@@ -0,0 +1,57 @@
+import { useCallback, useEffect, useState } from 'react';
+import { getValidationErrors } from '../utils/validation';
+
+
+const useForm = (schema, initialData) => {
+    const [isTouched, setIsTouched] = useState(false);
+    const [values, setValues] = useState({ ...initialData });
+    const [errors, setErrors] = useState({});
+
+    const handleChange = (e) => {
+        setValues({
+            ...values,
+            [e.target.name]: e.target.value,
+        });
+    };
+
+    const validate = useCallback((values, onSuccess) => {
+        schema.validate(values, { abortEarly: false })
+            .then(() => {
+                if (onSuccess) {
+                    onSuccess();
+                } else {
+                    // no errors and no success method, make sure we clear errors
+                    setErrors({});
+                }
+            }).catch((err) => {
+                setErrors(getValidationErrors(err));
+            });
+    }, [schema]);
+
+    // wrapper method for handling submit
+    // this way, we don't have to pass success callback in useForm constructor
+    const handleSubmit = (callback) => (e) => {
+        e.preventDefault();
+        setIsTouched(true);
+        validate(values, () => {
+            callback(values);
+        })
+    };
+
+    useEffect(() => {
+        if (isTouched) {
+            console.log(values);
+            validate(values);
+        }
+    }, [validate, isTouched, values]);
+
+    return {
+        isTouched,
+        values,
+        errors,
+        handleChange,
+        handleSubmit,
+    }
+};
+
+export default useForm;
